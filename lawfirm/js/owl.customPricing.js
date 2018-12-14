45a34cc0d193149e78027cf9dfff6dffc79b85a9
DCombs <dancombs94@gmail.com>
2018-12-14 10:36:13 -0500

    Begin working on Header Footer templates for reuse across html pages

---
+++ lawfirm/js/owl.customPricing.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    // center:true,
+    stagePadding: 50,
+    autowidth: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
