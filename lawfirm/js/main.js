0991f88bcd2246bf8dae5afdaa495259d1a138d1
DCombs <dancombs94@gmail.com>
2018-12-15 16:51:04 -0500

    Finalize function to stop video playback upon closing modal.

--- lawfirm/js/main.js
+++ lawfirm/js/main.js
@@ -299,5 +299,13 @@
 		fullHeight();
 	});
 
-
+	$(function(){
+		$("body").on('hidden.bs.modal', function (e) {
+		  var $iframes = $(e.target).find("iframe");
+		  $iframes.each(function(index, iframe){
+			$(iframe).attr("src", $(iframe).attr("src"));
+		  });
+		});
+	  });
+	  
 }());
